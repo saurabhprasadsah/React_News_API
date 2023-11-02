4311ffaf8dc5a431db92fe7019b18dd08cf1a75c
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-02 10:59:52 +0530

    reactapp

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
                 <ul className="dropdown-menu">
                   <li><a className="dropdown-item" href="#">Action</a></li>
                   <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" ></li>
+                  <li><hr className="dropdown-divider" /></li>
                   <li><a className="dropdown-item" href="#">Something else here</a></li>
                 </ul>
               </li>
