682aafd2efb1bbc1f66cb097244e08a5e4e90139
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-03 19:56:14 +0530

    reactapp

--- src/redux/reducer/news.reducer.js
+++ src/redux/reducer/news.reducer.js
@@ -17,26 +17,10 @@ const initialstate = {
     ]
 }
 
-// export const NewsReducer = (state = initialstate, action) => {
-//     switch (action.type) {
-//         // eslint-disable-next-line no-undef
-//         case GET_NEWS_SUCCESS:
-//             console.log(action.payload);
-//             return {
-//                 ...state,
-//                 news: [...action.payload]
-
-//             }
-
-//         default:
-//             return state;
-//     }
-// }
-
-
 export const NewsReducer = (state = initialstate, action) => {
     switch (action.type) {
         case GET_NEWS_SUCCESS: 
+        
             return {
                 ...state,
                 news: [...action.payload]
