6492713b535b5cae599b7527ee575385512ce2a6
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-03 19:13:31 +0530

    reactapp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,35 @@
+// import React, { useEffect } from 'react'
+// import NewsItem from '../components/NewsItem'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { getNewsStart } from '../redux/action/news.action';
+
+// export default function Home() {
+//   const news = useSelector(state => state.news);
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(getNewsStart())
+
+//   }, [dispatch])
+
+
+
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         {
+//           news.length > 0 && news.map((n, i) => (
+//             <NewsItem key={i} />
+//           ))
+//         }
+//       </div>
+
+//     </div>
+
+//   )
+// }
+
+
+
 import React, { useEffect } from 'react'
 import NewsItem from '../components/NewsItem'
 import { useDispatch, useSelector } from 'react-redux'
@@ -6,24 +38,21 @@ import { getNewsStart } from '../redux/action/news.action';
 export default function Home() {
   const news = useSelector(state => state.news);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getNewsStart())
-
-  }, [dispatch])
-
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="container">
-      <div className="row">
-        {
-          news.length > 0 && news.map((n, i) => (
-            <NewsItem key={i} />
-          ))
-        }
+        <div className="row">
+            {
+                news.length > 0 && news.map((n,i) => (
+                    <NewsItem data={n} key={i}/>
+                ))
+            }
+        </div>
       </div>
-
-    </div>
-
   )
 }
