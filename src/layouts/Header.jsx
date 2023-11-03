ec4a8db2c270664c9f5413efb64763e829a7f953
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-03 22:03:22 +0530

    reactapp

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,11 +4,10 @@ import { NavLink } from 'react-router-dom';
 
 export default function Header() {
     let category = useSelector(state => state.category);
-
     return (
         <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
             <div className="container">
-                <NavLink className="navbar-brand" to="/">News APP</NavLink>
+                <NavLink className="navbar-brand" to="/">News App</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -21,7 +20,6 @@ export default function Header() {
                                 </li>
                             ))
                         }
-
                     </ul>
                 </div>
             </div>
