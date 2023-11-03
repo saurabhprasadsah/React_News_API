039a04db0ca08fb10c6352a13d978e748b3005c3
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-03 19:08:18 +0530

    newsapp

--- src/redux/reducer/news.reducer.js
+++ src/redux/reducer/news.reducer.js
@@ -1,9 +1,6 @@
 import { GET_NEWS_SUCCESS } from "../constant/news.constant";
 
-
-
 const initialstate = {
-
     news: [],
     categoryNews: [],
     category: [
@@ -20,17 +17,32 @@ const initialstate = {
     ]
 }
 
+// export const NewsReducer = (state = initialstate, action) => {
+//     switch (action.type) {
+//         // eslint-disable-next-line no-undef
+//         case GET_NEWS_SUCCESS:
+//             console.log(action.payload);
+//             return {
+//                 ...state,
+//                 news: [...action.payload]
+
+//             }
+
+//         default:
+//             return state;
+//     }
+// }
+
+
 export const NewsReducer = (state = initialstate, action) => {
     switch (action.type) {
-        // eslint-disable-next-line no-undef
-        case GET_NEWS_SUCCESS:
-            console.log(action.payload);
+        case GET_NEWS_SUCCESS: 
             return {
                 ...state,
                 news: [...action.payload]
-
             }
 
+    
         default:
             return state;
     }
