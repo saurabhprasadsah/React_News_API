053ae384a90f363a5a4929a090be319b9430336d
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-03 19:58:06 +0530

    reactapp

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import NewsItem from '../components/NewsItem'
 
 export default function Category() {
+
   return (
     <div className="container">
       <div className="row">
+
         <NewsItem />
         <NewsItem />
         <NewsItem />
@@ -12,6 +14,7 @@ export default function Category() {
         <NewsItem />
         <NewsItem />
         <NewsItem />
+        
       </div>
     </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,3 @@
-// import React, { useEffect } from 'react'
-// import NewsItem from '../components/NewsItem'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { getNewsStart } from '../redux/action/news.action';
-
-// export default function Home() {
-//   const news = useSelector(state => state.news);
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(getNewsStart())
-
-//   }, [dispatch])
-
-
-
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         {
-//           news.length > 0 && news.map((n, i) => (
-//             <NewsItem key={i} />
-//           ))
-//         }
-//       </div>
-
-//     </div>
-
-//   )
-// }
-
-
-
 import React, { useEffect } from 'react'
 import NewsItem from '../components/NewsItem'
 import { useDispatch, useSelector } from 'react-redux'
