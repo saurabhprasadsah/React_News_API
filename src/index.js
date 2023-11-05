0dd9848815ef4f96ab68ee09e7b0cf5e80785ece
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-05 21:25:41 +0530

    finsh

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -16,7 +14,5 @@ root.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
-
-
 );
 
